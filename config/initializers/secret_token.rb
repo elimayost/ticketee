1a3d74abb8d514b9d4efbb63a3d5c3df6fb5f521
Eli Mayost <elimayost@localhost.(none)>
2010-08-27 11:17:18 +0100

    Created Rails 3 app and run rspec and cucumber generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '5c7bb606c94033fef86d486e2f9e650616e9f85a73692aa37363568200543d695480c7a918eaaf1049ec2112f8765fbbedf738d24374440de5bcc30ee191bfd6'
